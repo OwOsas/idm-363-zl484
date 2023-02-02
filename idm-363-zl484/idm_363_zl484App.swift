5354c2a5c3316379daf26ebd8977eec72db31e9c
Sylar Li | Zhengtian Li <20000620lzt@gmail.com>
2023-02-01 19:25:05 -0500

    Initial Commit

---
+++ idm-363-zl484/idm_363_zl484App.swift
@@ -0,0 +1,17 @@
+//
+//  idm_363_zl484App.swift
+//  idm-363-zl484
+//
+//  Created by Zhengtian Li on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct idm_363_zl484App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
