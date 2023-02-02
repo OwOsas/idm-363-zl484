3ae42b329dd4ab1745d3c2073017f53d16694e23
Sylar Li | Zhengtian Li <20000620lzt@gmail.com>
2023-02-02 00:05:17 -0500

    Setting Page, size optimization

---
+++ idm-363-zl484/HomeView.swift
@@ -0,0 +1,91 @@
+//
+//  HomeView.swift
+//  idm-363-zl484
+//
+//  Created by Zhengtian Li on 2/1/23.
+//
+
+import SwiftUI
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice(PreviewDevice(rawValue: "iPhone 8"))
+            .previewDisplayName("iPhone 8")
+        
+        ContentView()
+            .previewDevice(PreviewDevice(rawValue: "iPhone 14 Pro Max"))
+            .previewDisplayName("iPhone 14 Pro Max")
+    }
+}
+
+struct HomeView: View {
+    @Binding var colorPalette:[Color]
+    @Binding var paletteCount:Int
+    
+    var body: some View {
+        NavigationView{
+            ScrollView{
+                VStack(spacing: 24){
+                    VStack(spacing: 16){
+                        HStack{
+                            Text("Generate")
+                                .font(.title)
+                                .fontWeight(.bold)
+                            Spacer ()
+                        }
+                        
+                        
+                        RoundedRectangle(cornerRadius: 8)
+                            .foregroundColor(.white)
+                            .overlay(
+                                Image(systemName: "photo")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 64)
+                                    .foregroundColor(.secondary)
+                            )
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                            .aspectRatio(1.6, contentMode: .fit)
+                            .shadow(color: Color.black.opacity(0.1 ), radius: 5, x: 0, y: 5)
+                        
+                        HStack(spacing: 16){
+                            Button{
+                                
+                            }label: {
+                                Spacer()
+                                Image(systemName: "folder")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height: 21)
+                                Spacer()
+                            }
+                            .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 5)
+                            .buttonStyle(.borderedProminent)
+                            
+                            Button{
+                                
+                            }label: {
+                                Spacer()
+                                Image(systemName: "camera")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height: 21)
+                                Spacer()
+                            }
+                            .shadow(color: Color.black.opacity(0.1), radius: 5, x: 0, y: 5)
+                            .buttonStyle(.borderedProminent)
+                        }
+                    }
+                    
+                    ColorPaletteView(colorPalette: self.$colorPalette, paletteCount: self.$paletteCount)
+                    Spacer()
+                }
+                .padding(20) 
+            }
+        }
+        .tabItem{
+            Image(systemName: "house")
+            Text("Home")
+        }
+    }
+}
---
+++ idm-363-zl484/SettingView.swift
@@ -0,0 +1,45 @@
+//
+//  SettingView.swift
+//  idm-363-zl484
+//
+//  Created by Zhengtian Li on 2/1/23.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    @Binding var colorPalette: [Color]
+    @Binding var paletteCount:Int
+    
+    var body: some View {
+        NavigationView{
+            Form{
+                Section(header: Text("Color Mode")){
+                    Toggle(isOn: .constant(true),
+                           label: {Text("Dark Mode")})
+                }
+                Section(header: Text("Default Settings")){
+                    Picker(selection: /*@START_MENU_TOKEN@*/.constant(1)/*@END_MENU_TOKEN@*/, label: Text("Copy Format")) {
+                        Text("HEX").tag(1)
+                        Text("RGB").tag(2)
+                    }
+                    
+                    Stepper(value: $paletteCount, in: 5...10) {
+                        Text("Palette Count \(paletteCount)")
+                    }
+                }
+            }.navigationTitle(Text("Settings"))
+        }
+        .tabItem{
+            Image(systemName: "gear")
+            Text("Settings")
+        }
+    }
+    
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
