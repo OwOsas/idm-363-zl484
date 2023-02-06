8009ca6cdb0a7c7e82e45c083b0602e005d8c8d0
Sylar Li | Zhengtian Li <20000620lzt@gmail.com>
2023-02-06 17:39:42 -0500

    Import ColorKit package

---
+++ idm-363-zl484/ColorKitTest.swift
@@ -0,0 +1,21 @@
+//
+//  ColorKitTest.swift
+//  idm-363-zl484
+//
+//  Created by Zhengtian Li on 2/6/23.
+//
+
+import SwiftUI
+import ColorKit
+
+struct ColorKitTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ColorKitTest_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorKitTest()
+    }
+}
