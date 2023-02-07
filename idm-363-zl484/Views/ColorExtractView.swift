eca9b9d6c5afb7024023a97ccda51762f763d4f5
Sylar Li | Zhengtian Li <20000620lzt@gmail.com>
2023-02-07 01:10:46 -0500

    Successfully got an array of color based on imp

--- idm-363-zl484/Views/ColorExtractView.swift
+++ idm-363-zl484/Views/ColorExtractView.swift
@@ -14,15 +14,36 @@ struct ColorExtractView: View {
     @State private var colorArr:[Color] = []
     
     var body: some View {
-        let colors = getDominantColor(img: loadImg)
-//        Text("\(colors[4].frequency)")
+        ScrollView{
+            VStack{
+                let colors = getDominantColor(img: loadImg)
+//                Text(colors.description)
+                let colorPalette = getColorPalette(ColorFrequencyArray: colors, ArrayCount: getSize(arr: colors))
+                HStack{
+                    ForEach(colorPalette, id: \.self){color in
+                        Rectangle()
+                            .frame(width: 30, height: 30)
+                            .aspectRatio(1, contentMode: .fit)
+                        .overlay(Color(uiColor: color))}
+                }
+                ForEach(colorPalette, id: \.self){color in
+                    Rectangle()
+                        .frame(width: 30, height: 30)
+                        .aspectRatio(1, contentMode: .fit)
+                        .overlay(Color(uiColor: color))
+                    Text("\(color.description)")
+                }
+        //        Text("\(colors[4].frequency)")
 
+                
+                Rectangle()
+                    .frame(width: 300, height: 300)
+                    .aspectRatio(1, contentMode: .fit)
+                    .overlay(Color(uiColor: colors[2].color))
+        //        self.$colorArr = extractColors(from: CIImage(image: loadImg))
+            }
+        }
         
-        Rectangle()
-            .frame(width: 300, height: 300)
-            .aspectRatio(1, contentMode: .fit)
-            .overlay(Color(uiColor: colors[2].color))
-//        self.$colorArr = extractColors(from: CIImage(image: loadImg))
     }
     
     func getDominantColor(img:UIImage) -> [ColorFrequency]{
@@ -39,6 +60,19 @@ struct ColorExtractView: View {
         var dominantColors = [ColorFrequency]()
         currentImage = img
         return dominantColors
+    }
+    
+    func getColorPalette(ColorFrequencyArray arr: [ColorFrequency], ArrayCount count: Int) -> [UIColor]{
+        var colorPalette = [UIColor]()
+        for i in 0..<count{
+            colorPalette += [arr[i].color]
+        }
+        
+        return colorPalette
+    }
+    
+    func getSize(arr:[Any]) -> Int{
+        return arr.count
     }
 }
 
