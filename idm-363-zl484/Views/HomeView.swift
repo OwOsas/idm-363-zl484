d2e8aabb5d88a14b45421fe83655ebbd534bb5c5
Sylar Li | Zhengtian Li <20000620lzt@gmail.com>
2023-03-24 00:27:23 -0400

    Toast notification on palette copy

--- idm-363-zl484/Views/HomeView.swift
+++ idm-363-zl484/Views/HomeView.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import SimpleToast
+
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
@@ -22,173 +24,180 @@ struct HomeView: View {
     @Binding var paletteCount:Int
     @Binding var selectedImg:UIImage?
     
-    @State var isPickerShowing = false 
+    @State var isPickerShowing = false
     @State var isCameraShowing = false
     @State var isActionSheetShowing = false
     @State var isNotificationShowing = false
+    
+    private let toastOptions = SimpleToastOptions(
+        alignment: .top,
+        hideAfter: 2,
+        animation: .default,
+        modifierType: .slide
+    )
     
     private let pastboard = UIPasteboard.general
     
     var body: some View {
         NavigationView{
-            ZStack{
-                VStack(spacing: 28){
-                    VStack(spacing: 16){
-                        //Image Display
-                        if selectedImg != nil{
-                            VStack{
-                                Image(uiImage: selectedImg!)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .cornerRadius(8)
-                            }
-                            .frame(minWidth: 0, maxWidth: .infinity)
-                            .aspectRatio(1.6, contentMode: .fit)
-                            .background(
-                                RoundedRectangle(cornerRadius: 8)
-                                    .foregroundColor(.white)
-                                    .frame(minWidth: 0, maxWidth: .infinity)
-                                    .aspectRatio(1.6, contentMode: .fit)
-                            )
+            
+            VStack(spacing: 28){
+                VStack(spacing: 16){
+                    //Image Display
+                    if selectedImg != nil{
+                        VStack{
+                            Image(uiImage: selectedImg!)
+                                .resizable()
+                                .scaledToFit()
+                                .cornerRadius(8)
                         }
-                        else{
-                            VStack{
-                                Spacer()
-                                Image(systemName: "photo")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(maxWidth: 64)
-                                    .foregroundColor(Color("Border"))
-                                
-                                Spacer()
-                            }
-                            .frame(minWidth: 0, maxWidth: .infinity)
-                            .aspectRatio(1.6, contentMode: .fit)
-                            .background(
-                                RoundedRectangle(cornerRadius: 8)
-                                    .foregroundColor(.white)
-                                    .frame(minWidth: 0, maxWidth: .infinity)
-                                    .aspectRatio(1.6, contentMode: .fit)
-                            )
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 8)
-                                    .strokeBorder(Color("Border"), style: StrokeStyle(lineWidth: 2, dash: [5, 2]))
-                                    .foregroundColor(.clear)
-                                    .frame(minWidth: 0, maxWidth: .infinity)
-                                    .aspectRatio(1.6, contentMode: .fit)
-                            )
-                        }
-                        
-                        //Image Display End
-                        
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .aspectRatio(1.6, contentMode: .fit)
+                        .background(
+                            RoundedRectangle(cornerRadius: 8)
+                                .foregroundColor(.white)
+                                .frame(minWidth: 0, maxWidth: .infinity)
+                                .aspectRatio(1.6, contentMode: .fit)
+                        )
                     }
-                    .padding([.leading, .trailing], 20)
-                    
-                    
-                    ColorPaletteWidget(colorPalette: self.$colorPalette, paletteCount: self.$paletteCount, selectedImg: self.$selectedImg, isNotificationShowing: self.$isNotificationShowing)
-                    
-                    VStack(spacing: 16){
-                        Button{
-                            //                        selectedImg = UIImage(named: "TestImg_06")
-                            isActionSheetShowing = true
-                            print("Button Clicked")
-                        }label: {
-                            HStack(spacing: 20){
-                                Image(systemName: "photo")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 28)
-                                Spacer()
-                                
-                                Text("New Image")
-                                Spacer()
-                                Spacer()
-                                    .frame(width: 28)
-                                
-                            }
+                    else{
+                        VStack{
+                            Spacer()
+                            Image(systemName: "photo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxWidth: 64)
+                                .foregroundColor(Color("Border"))
+                            
+                            Spacer()
                         }
-                        .background(.clear)
-                        .foregroundColor(Color("Text"))
-                        .padding([.leading, .trailing], 25)
-                        .padding([.top, .bottom], 10)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .aspectRatio(1.6, contentMode: .fit)
+                        .background(
+                            RoundedRectangle(cornerRadius: 8)
+                                .foregroundColor(.white)
+                                .frame(minWidth: 0, maxWidth: .infinity)
+                                .aspectRatio(1.6, contentMode: .fit)
+                        )
                         .overlay(
                             RoundedRectangle(cornerRadius: 8)
                                 .strokeBorder(Color("Border"), style: StrokeStyle(lineWidth: 2, dash: [5, 2]))
                                 .foregroundColor(.clear)
                                 .frame(minWidth: 0, maxWidth: .infinity)
+                                .aspectRatio(1.6, contentMode: .fit)
                         )
-                        
-                        
-//                        Button{
-//
-//                        }label: {
-//                            Spacer()
-//                            Text("SAVE PALETTE")
-//                            Spacer()
-//                        }
-//                        .padding(12)
-//                        .foregroundColor(.white)
-//                        .background(
-//                            RoundedRectangle(cornerRadius: 8)
-//                                .foregroundColor(Color("Button"))
-//                                .frame(minWidth: 0, maxWidth: .infinity)
-//                        )
-                        
                     }
-                    .padding([.leading, .trailing], 20)
-                    .actionSheet(isPresented: $isActionSheetShowing, content: {()-> ActionSheet in
-                        ActionSheet(title: Text("New Image"),
-                                    buttons: [
-//                                        .default(
-//                                            Text("Camera"), action: {
-//                                                isCameraShowing = true
-//                                            }),
-                                        .default(
-                                            Text("Gallery"), action: {
-                                                isPickerShowing = true
-                                            }),
-                                        .cancel()
-                                    ]
-                        )
-                        
-                    })
-                    .sheet(isPresented: $isPickerShowing, content: {
-                        ImagePicker(selectedImg: $selectedImg, isCamera: false)
-                    })
-                    .fullScreenCover(isPresented: $isCameraShowing, content: {
-                        ImagePicker(selectedImg: $selectedImg, isCamera: true)
-                    })
+                    
+                    //Image Display End
+                    
                 }
-                .padding([.top, .bottom], 20)
-                .frame(
-                    minWidth: 0,
-                    maxWidth: .infinity,
-                    minHeight: 0,
-                    maxHeight: .infinity
-                )
+                .padding([.leading, .trailing], 20)
                 
-                if isNotificationShowing{
-                    VStack{
-                        VStack(spacing: 10){
+                
+                ColorPaletteWidget(colorPalette: self.$colorPalette, paletteCount: self.$paletteCount, selectedImg: self.$selectedImg, isNotificationShowing: self.$isNotificationShowing)
+                
+                VStack(spacing: 16){
+                    Button{
+                        //                        selectedImg = UIImage(named: "TestImg_06")
+                        isActionSheetShowing = true
+                        print("Button Clicked")
+                    }label: {
+                        HStack(spacing: 20){
+                            Image(systemName: "photo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 28)
+                            Spacer()
                             
-                            HStack{
-                                Image(systemName: "exclamationmark.circle")
-                                Text("Palette Colors Copied")
-                                
-                                Spacer()
-                            }
-                            .buttonStyle(.borderedProminent)
-                            .padding(16)
-                            .background(RoundedRectangle(cornerRadius: 12)
-                                .foregroundColor(Color.white)
-                                .shadow(color: Color.black.opacity(0.1 ), radius: 5, x: 0, y: 5))
+                            Text("New Image")
+                            Spacer()
+                            Spacer()
+                                .frame(width: 28)
+                            
                         }
-                        .padding([.leading, .trailing], 20)
+                    }
+                    .background(.clear)
+                    .foregroundColor(Color("Text"))
+                    .padding([.leading, .trailing], 25)
+                    .padding([.top, .bottom], 10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                            .strokeBorder(Color("Border"), style: StrokeStyle(lineWidth: 2, dash: [5, 2]))
+                            .foregroundColor(.clear)
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                    )
+                    
+                    
+                    //                        Button{
+                    //
+                    //                        }label: {
+                    //                            Spacer()
+                    //                            Text("SAVE PALETTE")
+                    //                            Spacer()
+                    //                        }
+                    //                        .padding(12)
+                    //                        .foregroundColor(.white)
+                    //                        .background(
+                    //                            RoundedRectangle(cornerRadius: 8)
+                    //                                .foregroundColor(Color("Button"))
+                    //                                .frame(minWidth: 0, maxWidth: .infinity)
+                    //                        )
+                    
+                }
+                .padding([.leading, .trailing], 20)
+                .actionSheet(isPresented: $isActionSheetShowing, content: {()-> ActionSheet in
+                    ActionSheet(title: Text("New Image"),
+                                buttons: [
+                                    //                                        .default(
+                                    //                                            Text("Camera"), action: {
+                                    //                                                isCameraShowing = true
+                                    //                                            }),
+                                    .default(
+                                        Text("Gallery"), action: {
+                                            isPickerShowing = true
+                                        }),
+                                    .cancel()
+                                ]
+                    )
+                    
+                })
+                .sheet(isPresented: $isPickerShowing, content: {
+                    ImagePicker(selectedImg: $selectedImg, isCamera: false)
+                })
+                .fullScreenCover(isPresented: $isCameraShowing, content: {
+                    ImagePicker(selectedImg: $selectedImg, isCamera: true)
+                })
+            }
+            .padding([.top, .bottom], 20)
+            .frame(
+                minWidth: 0,
+                maxWidth: .infinity,
+                minHeight: 0,
+                maxHeight: .infinity
+            )
+            .simpleToast(isPresented: $isNotificationShowing, options: toastOptions) {
+                VStack(spacing: 10){
+                    
+                    HStack{
+                        Image(systemName: "exclamationmark.circle")
+                            .foregroundColor(.white)
+                            .bold()
+                        Text("Palette Colors Copied")
+                            .foregroundColor(.white)
+                            .bold()
                         
                         Spacer()
                     }
+                    .buttonStyle(.borderedProminent)
+                    .padding(16)
+                    .background(RoundedRectangle(cornerRadius: 12)
+                        .foregroundColor(Color("Button"))
+                        .shadow(color: Color.black.opacity(0.1 ), radius: 5, x: 0, y: 5))
                 }
+                .padding([.leading, .trailing], 20)
             }
+            
+            
             
         }
         .tabItem{
